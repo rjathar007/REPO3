b4775d1fde24f082b8ca24abfe9a445696ba8546
rjathar007 <rjathar878@gmail.com>
2020-12-05 01:47:57 +0530

    Third Project

---
+++ src/main/java/GITDEMO/GITDEMO/App.java
@@ -0,0 +1,13 @@
+package GITDEMO.GITDEMO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
